51b7b161527ac974e4167a56cd782e2eee775a40
fredmeteor <fredpeter09899@gmail.com>
2023-05-31 21:09:00 +0300

    Added PermissionSeed

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
 use Illuminate\Database\Seeder;
-
+use App\Models\Permission;
 class PermissionSeeder extends Seeder
 {
     /**
@@ -13,6 +14,19 @@ class PermissionSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $allRoles = Role::all()->keyBy('id');
+
+        $permissions = [
+            'properties-manage' => [Role::ROLE_OWNER],
+            'bookings-manage' => [Role::ROLE_USER],
+        ];
+
+        foreach ($permissions as $key => $roles) {
+            $permission = Permission::create(['name' => $key]);
+            foreach ($roles as $role) {
+                $allRoles[$role]->permissions()->attach($permission->id);
+            }
+        }
+        
     }
 }
