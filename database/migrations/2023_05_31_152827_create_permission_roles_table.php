d5981bee67427b5ce935f8f74133edba87c8482f
fredmeteor <fredpeter09899@gmail.com>
2023-05-31 18:31:29 +0300

    Added Permisions_Model

--- database/migrations/2023_05_31_152827_create_permission_roles_table.php
+++ database/migrations/2023_05_31_152827_create_permission_roles_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateRoleUsersTable extends Migration
+class CreatePermissionRolesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,9 @@ class CreateRoleUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('role_users', function (Blueprint $table) {
-            $table->foreignId('user_id')->constrained();
-            $table->foreignId('user_id')->constrained();
-            $table->timestamps();
+        Schema::create('permission_roles', function (Blueprint $table) {
+           $ $table->foreignId('permission_id')->constrained();
+           $table->foreignId('role_id')->constrained(); 
         });
     }
 
@@ -27,6 +26,6 @@ class CreateRoleUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('role_users');
+        Schema::dropIfExists('permission_roles');
     }
 }
--- database/seeders/RoleUserSeeder.php
+++ database/seeders/RoleUserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
 use Illuminate\Database\Seeder;
 
 class RoleUserSeeder extends Seeder
@@ -13,6 +14,8 @@ class RoleUserSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Role::create(['name' => 'Administrator']);
+        Role::create(['name' => 'Property Owner']);
+        Role::create(['name' => 'Simple User']);
     }
 }
