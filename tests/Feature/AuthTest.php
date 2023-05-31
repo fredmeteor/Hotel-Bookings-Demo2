543d2fce0c1aefb95847ee2c904d308f7f877927
fredmeteor <fredpeter09899@gmail.com>
2023-05-31 20:25:41 +0300

    Fixed incorect Foreign  key constraint due no role_id in users table and RoleUser and PermissionRole PivitTables

---
+++ tests/Feature/AuthTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Role;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class AuthTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    use RefreshDatabase;
+
+    public function test_registration_fails_with_admin_role()
+    {
+        $response = $this->postJson('/api/auth/register', [
+            'name' => 'Valid name',
+            'email' => 'valid@email.com',
+            'password' => 'ValidPassword',
+            'password_confirmation' => 'ValidPassword',
+            'role_id' => Role::ROLE_ADMINISTRATOR
+        ]);
+
+        $response->assertStatus(422);
+    }
+
+    public function test_registration_succeeds_with_owner_role()
+    {
+        $response = $this->postJson('/api/auth/register', [
+            'name' => 'Valid name',
+            'email' => 'valid@email.com',
+            'password' => 'ValidPassword',
+            'password_confirmation' => 'ValidPassword',
+            'role_id' => Role::ROLE_OWNER
+        ]);
+
+        $response->assertStatus(200)->assertJsonStructure([
+            'access_token',
+        ]);
+    }
+
+    public function test_registration_succeeds_with_user_role()
+    {
+        $response = $this->postJson('/api/auth/register', [
+            'name' => 'Valid name',
+            'email' => 'valid@email.com',
+            'password' => 'ValidPassword',
+            'password_confirmation' => 'ValidPassword',
+            'role_id' => Role::ROLE_USER
+        ]);
+
+        $response->assertStatus(200)->assertJsonStructure([
+            'access_token',
+        ]);
+    }
+}
