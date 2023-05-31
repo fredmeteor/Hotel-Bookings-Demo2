4a03dae8a07d81767b2d283a6d184889c603204d
fredmeteor <fredpeter09899@gmail.com>
2023-05-31 23:29:20 +0300

    Added City and GeoObject/Country Seeders

--- database/seeders/GeoobjectSeeder.php
+++ database/seeders/GeoobjectSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Geoobject;
 use Illuminate\Database\Seeder;
 
 class GeoobjectSeeder extends Seeder
@@ -13,6 +14,18 @@ class GeoobjectSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Geoobject::create([
+            'city_id' => 1,
+            'name' => 'Statue of Liberty',
+            'lat' => 40.689247,
+            'long' => -74.044502
+        ]);
+
+        Geoobject::create([
+            'city_id' => 2,
+            'name' => 'Big Ben',
+            'lat' => 51.500729,
+            'long' => -0.124625
+        ]);
     }
 }
